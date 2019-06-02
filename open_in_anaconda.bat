704ed3343411070737fe8e7ff7e9dbaf86977202
dima <dmitrijbozhkov@gmail.com>
2019-06-02 17:10:55 +0300

    fix bat

--- open_in_anaconda.bat
+++ open_in_anaconda.bat
@@ -1,11 +1,7 @@
-set root=C:\Users\Yashi\Miniconda3
+set root=/path/to/anaconda/folder/
 
 call %root%\Scripts\activate.bat %root%
 
-call python -m social_media_scraper -m id -i "./example-identification.csv" -o "./_temp.csv" -lb 1 -ub 3 -g "C:\Users\Yashi\Documents\geckodriver.exe"
-
-call python -m social_media_scraper -m acc -hi -i "./_temp.csv" -o "./scraping_result.db" -lb 1 -ub 3 -g "C:\Users\Yashi\Documents\geckodriver.exe"
-
-call del /f _temp.csv
+call python -m social_media_scraper -m full -i "./example-identification.csv" -o "./output.db" -lb 1 -ub 3 -d -s -g "/path/to/driver/geckodriver.exe"
 
 pause
